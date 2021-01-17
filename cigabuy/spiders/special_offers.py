31fbb07a0cd2cd77d406704d9c18f08f9c56aee9
Vivek Kumar <32337899+vickytilotia@users.noreply.github.com>
2021-01-17 21:29:47 +0530

    Scraping Multiple Pages

--- cigabuy/spiders/special_offers.py
+++ cigabuy/spiders/special_offers.py
@@ -14,6 +14,13 @@ class SpecialOffersSpider(scrapy.Spider):
                 'title' : product.xpath(".//a[@class = 'p_box_title']/text()").get(),
                 #using urljoin as the url is relative.
                 'url' : response.urljoin(product.xpath(".//a[@class = 'p_box_title']/@href").get()),
-                'price' : product.xpath(".//div[@class = 'p_box_price cf']/text()").get(),
+                'discounted_price' : product.xpath(".//div[@class = 'p_box_price cf']/span[1]/text()").get(),
+                'original_price' : product.xpath(".//div[@class = 'p_box_price cf']/span[2]/text()").get()
                 
             }
+        # extracting next pages
+        next_page = response.xpath("//a[@class='nextPage']/@href").get()
+
+        #check if the next page is exist or not!
+        if next_page:
+            yield scrapy.Request(url = next_page, callback=self.parse)
